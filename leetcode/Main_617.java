a233f942d04bc677328eac152d6541a726dfbaea
Bomi <a51022866@gmail.com>
2020-08-20 10:45:36 +0900

    2020 08 20 solve DFS

---
+++ leetcode/Main_617.java
@@ -0,0 +1,38 @@
+package me.algo.leetcode;
+
+/**
+ * Created by Bomi on 2020/08/20.
+ * 문제 출처 : https://leetcode.com/problems/merge-two-binary-trees/
+ *
+ * Time Complexity : O(N)
+ * Used Algorithm : DFS
+ */
+
+public class Main_617 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
