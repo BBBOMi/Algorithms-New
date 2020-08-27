be4342ec2798217eb710098a24ce1f67e48f94e4
Bomi <a51022866@gmail.com>
2020-08-27 19:54:17 +0900

    2020 08 27 solve String 자료구조

---
+++ week15/programmers/Main_64065.java
@@ -0,0 +1,38 @@
+package me.algo.week15.programmers;
+
+import java.util.*;
+
+/**
+ * Created by Bomi on 2020/08/27.
+ * 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/64065
+ *
+ * Time Complexity : O(N)
+ * Used Algorithm : String, 자료구조
+ * Used Data structure : Array, Set(LinkedHashSet)
+ */
+
+public class Main_64065 {
+    public int[] solution(String s) {
+        s = s.replaceAll("[{]", "");
+        String[] arr = s.split("}");
+        Arrays.sort(arr, Comparator.comparingInt(String::length));
+
+        Set<String> set = new LinkedHashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            String str = arr[i].trim();
+            String temp = "";
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) != ',') {
+                    temp += str.charAt(j);
+                }
+                if (str.charAt(j) == ',' || j == str.length() - 1) {
+                    if (temp != "") {
+                        set.add(temp);
+                        temp = "";
+                    }
+                }
+            }
+        }
+        return set.stream().mapToInt(Integer::parseInt).toArray();
+    }
+}
