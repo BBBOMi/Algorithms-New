35d1e41f6394977b07ded1180fef82badcd4759d
Bomi <a51022866@gmail.com>
2020-12-25 17:13:20 +0900

    2020 12 25 solve Minimax

---
+++ week32/Leet877.kt
@@ -0,0 +1,15 @@
+package me.algo.week32
+
+/**
+ * Created by Bomi on 2020/12/25.
+ * 문제 출처 : https://leetcode.com/problems/stone-game/
+ *
+ * Time Complexity : O(1)
+ * Used Algorithm : minimax
+ */
+
+class Leet877 {
+    fun stoneGame(piles: IntArray): Boolean {
+        return true
+    }
+}
