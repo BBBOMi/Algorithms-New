65b78eee8bbe152ef81410109a9d60f1323e7383
Bomi <k.bomi096@gmail.com>
2021-04-05 12:38:44 +0900

    2021 04 05 solve Math

---
+++ week48/wed/BOJ1934.java
@@ -0,0 +1,38 @@
+package me.algo.week48.wed;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Bomi on 2021/04/05.
+ * 문제 출처 : https://www.acmicpc.net/problem/1934
+ *
+ * Time Complexity : O(N)
+ * Used Algorithm : Math
+ */
+
+public class BOJ1934 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(lcm(a, b) + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    private static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
