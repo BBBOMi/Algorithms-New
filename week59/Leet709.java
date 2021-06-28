a2ea3b9081aa05fd562c8823dbe98ef92b3b4606
Bomi <k.bomi096@gmail.com>
2021-06-28 14:58:53 +0900

    2021 06 28 solve String

---
+++ week59/Leet709.java
@@ -0,0 +1,15 @@
+package me.algo.week59;
+
+/**
+ * Created by Bomi on 2021/06/28.
+ * 문제 출처 : https://leetcode.com/problems/to-lower-case/
+ *
+ * Time Complexity : O(N)
+ * Used Algorithm : String
+ */
+
+public class Leet709 {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
